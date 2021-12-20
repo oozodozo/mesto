94dcc4cfcde6ed2e2e4b3a3ce5553631022bb9b1
Ivan Dozorov <oozo1101@yandex.ru>
2021-12-20 19:55:02 +0400

    feat: Добавить javascript для popup

---
+++ pages/index.js
@@ -0,0 +1,29 @@
+let popup = document.querySelector('.popup'); //popup
+let profileEditButton = document.querySelector('.profile__edit-button'); // кнопка вызова popup
+let popupReset = document.querySelector('.popup__button-reset'); // кнопка закрытия popup
+let formElement = document.querySelector('.popup__form'); // форма редактирование имени
+let nameImput = formElement.querySelector('.popup__user-name'); // input для ввода имени
+let jobInput = formElement.querySelector('.popup__user-about'); // input для описание пользователя
+let userName = document.querySelector('.profile__title'); // имя пользователья на странице
+let userJob = document.querySelector('.profile__description'); // описание пользователя на странице
+
+//Функция на открытие и закрытие popup
+function popupOpen () {
+  if (popup.classList.contains('popup_opened') === false) {
+    popup.classList.add('popup_opened');
+    nameImput.value = userName.textContent;
+    jobInput.value = userJob.textContent;
+  } else {
+    popup.classList.remove('popup_opened');
+  }
+}
+//Функция на сохранение имени и описания
+function formSubmitHandler (evt) {
+  evt.preventDefault();
+  userName.textContent = nameImput.value;
+  userJob.textContent = jobInput.value;
+}
+
+profileEditButton.addEventListener('click', popupOpen);
+popupReset.addEventListener('click', popupOpen);
+formElement.addEventListener('submit', formSubmitHandler);
